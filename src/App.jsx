5db39689cc0e3c77c8092ed577ad52c79681828e
Khushbu K. <khushukat@gmail.com>
2025-04-27 17:23:51 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import useCurrencyInfo from "./hooks/useCurrencyInfo";
 import InputBox from "./components/inputBox";
-
 function App() {
   const [amount, setAmount] = useState(0);
   const [from, setFrom] = useState("usd");
